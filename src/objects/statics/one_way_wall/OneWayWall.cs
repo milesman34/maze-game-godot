762c1ceeaecccce1fdf7cbb2a2ac74491acd80ee
milesman34 <milesmanmusic@yahoo.com>
2024-02-13 15:23:29 -0500

    Added one-way walls

--- src/objects/statics/one_way_wall/OneWayWall.cs
+++ src/objects/statics/one_way_wall/OneWayWall.cs
@@ -6,12 +6,16 @@ using System;
 /// </summary>
 [Tool]
 public partial class OneWayWall : RigidBody2D {
+	/// <summary>
+	/// Direction the wall is rotated
+	/// </summary>
 	[Export]
-	public Direction Direction { get; set; } = Direction.Right;
+	public Direction Direction { get; set; } = Direction.Up;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready() {
-		RotationDegrees = Direction.GetRotation();
+		// To make the one-way collisions work, I had to make the default rotation be up, so now I have to add 90 to this.
+		RotationDegrees = Direction.GetRotation() + 90;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
