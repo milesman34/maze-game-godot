ac4f2d1f39f6f3f1917f617d87e68e8bae1635b8
milesman34 <milesmanmusic@yahoo.com>
2024-02-13 11:02:19 -0500

    Moved C# files to src directory

---
+++ src/objects/statics/OneWayWall.cs
@@ -0,0 +1,13 @@
+using Godot;
+using System;
+
+/// <summary>
+/// Wall that only blocks against one direction.
+/// </summary>
+public partial class OneWayWall : RigidBody2D {
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready() {}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta) {}
+}
